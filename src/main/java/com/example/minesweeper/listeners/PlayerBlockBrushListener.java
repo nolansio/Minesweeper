2aaaa84d267e546a7b6d81d97820b75f0086c3bb
nolansio <nolanarsac105@gmail.com>
2025-12-10 20:57:09 +0100

    Correction du système de déminage V1 et modification des blocs vert clair en grès

--- src/main/java/com/example/minesweeper/listeners/PlayerBlockBrushListener.java
+++ src/main/java/com/example/minesweeper/listeners/PlayerBlockBrushListener.java
@@ -27,26 +27,35 @@ public class PlayerBlockBrushListener implements Listener {
             return;
         }
 
-        if (player.getGameMode() == GameMode.SURVIVAL) {
-            if (ChunkInfo.isChunkOf(player)) {
-                event.setCancelled(true);
-            }
+        event.setCancelled(true);
+
+        boolean isChunkOfPlayer = ChunkInfo.isChunkOf(player);
+
+        if (isChunkOfPlayer) {
+            player.sendMessage("Ton chunk");
+        } else {
+            player.sendMessage("Pas ton chunk");
         }
 
-        Block below = sand.getRelative(BlockFace.DOWN);
-        if (below.getType() == Material.TNT) {
-            below.setType(Material.SAND);
+        if (!player.isOp() && !isChunkOfPlayer) {
+            return;
+        }
+
+        Block sandstone = sand.getRelative(BlockFace.DOWN);
+        if (sandstone.getType() == Material.TNT) {
+            sandstone.setType(Material.SANDSTONE);
             sand.setType(Material.AIR);
 
             minesweeperWorld.playSound(player.getLocation(), Sound.ENTITY_TNT_PRIMED, 1f, 1f);
-            minesweeperWorld.spawn(sand.getLocation().add(0.5, 0, 0.5), TNTPrimed.class);
+            TNTPrimed tnt = minesweeperWorld.spawn(sand.getLocation().add(0.5, 0, 0.5), TNTPrimed.class);
+            tnt.setFuseTicks(40);
             return;
         }
 
         int tntCount = 0;
         for (int x = -1; x <= 1; x++) {
             for (int z = -1; z <= 1; z++) {
-                Block block = below.getRelative(x, 0, z);
+                Block block = sandstone.getRelative(x, 0, z);
                 if (block.getType() == Material.TNT) {
                     tntCount++;
                 }
@@ -66,9 +75,7 @@ public class PlayerBlockBrushListener implements Listener {
                 Material.GRAY_GLAZED_TERRACOTTA
         };
 
-        int i = Math.min(tntCount, blocks.length - 1);
-        sand.setType(blocks[i]);
-        System.out.println(i);
+        sand.setType(blocks[tntCount]);
     }
 
 }
